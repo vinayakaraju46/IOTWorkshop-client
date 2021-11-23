ac731a3a681de5829ead7f5fea79b11589d48786
vinayakarajus <vinayakaraju46@gmail.com>
2021-11-23 22:16:48 +0530

    Initial Commit

---
+++ src/views/Switchcomponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="6">
+      <v-card height="300" width="400" class="mt-10 mb-10" elevation="2">
+        <v-card-text class="text-h5 mb-1"> Devicename: {{ DeviceName }} </v-card-text>
+        <v-row>
+          <v-col>
+
+          </v-col>
+        </v-row>
+        <v-row justify="center" align="center">
+          <v-col cols="4">
+            <v-switch v-model="switch1" color="success" class="pl-8"></v-switch>
+          </v-col>
+        </v-row>
+        
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "switchComponent",
+  data() {
+    return {
+      DeviceName: "",
+      switch1: false,
+    };
+  },
+  mounted() {
+    
+  }
+};
+</script>
---
+++ src/views/temperature.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card flat>
+    <v-card-text class="text-h5 mb-1"> Temperature is {{ temp }}</v-card-text>
+    <v-card-text class="text-h5 mb-1"> Humidity is {{ humidity }}</v-card-text>
+  </v-card>
+</template>
+
+<script>
+const axios = require('axios');
+
+export default {
+    name: "temperature",
+    data() {
+        return {
+          temp: '',
+          humidity: ''
+        }
+    },
+    mounted() {
+      this.getWeather();
+    },
+    methods: {
+      async getWeather() {
+        axios.get('https://mocki.io/v1/35718433-8268-4eb6-a889-af8f595e8918')
+        .then((data) => {
+          this.temp = data.data.temp;
+          this.humidity = data.data.humidity;
+        })
+      }
+    }
+}
+</script>
