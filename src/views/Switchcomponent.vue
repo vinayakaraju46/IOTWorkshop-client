bbd19c81de830f11e475c35d18f365f9ce2a5bae
vinayakarajus <vinayakaraju46@gmail.com>
2021-11-24 23:22:15 +0530

    final push

--- src/views/Switchcomponent.vue
+++ src/views/Switchcomponent.vue
@@ -1,21 +1,36 @@
 <template>
   <v-row justify="center">
-    <v-col cols="6">
+    <v-col cols="6" v-if="devices.length">
       <v-row justify="center" v-for="(item, index) in devices" :key="index">
-        <v-col cols="3" align="center">
-          <p>Devicename: {{ item.deviceName }}</p>
+        <v-col cols="6" align="center" class="mt-5">
+          <p>MAC_ID: {{ item }}</p>
         </v-col>
-        <v-col cols="4">
+        <v-col cols="2">
           <v-switch
-            :value="true"
-            :input-value="item.deviceState == 'ON' ? true : false"
-            @change="toggle(item.deviceName, item.deviceState)"
+            v-model="switch1"
+            @change="toggle(item)"
             color="success"
             class="pl-8"
           ></v-switch>
         </v-col>
+        <v-col cols="4">
+          <v-btn
+            class="ma-1 mt-3"
+            color="error"
+            @click="remove(item)"
+          >
+            Delete
+          </v-btn>
+        </v-col>
       </v-row>
     </v-col>
+    <!-- <v-sheet
+      :max-width="model === 6 ? 128 : 256"
+      class="mx-auto transition-swing secondary circle"
+      elevation="12"
+      height="128"
+      width="100%"
+    ></v-sheet> -->
   </v-row>
 </template>
 
@@ -28,6 +43,7 @@ export default {
     return {
       DeviceName: "",
       switch1: false,
+      mac: "",
       body: {
         MAC_ID: 1,
       },
@@ -35,25 +51,40 @@ export default {
     };
   },
   mounted() {
-    axios.post("http://localhost:5000/getDevice", this.body).then((data) => {
-      console.log(data, "received data");
-      this.devices = data.data.devices;
-      console.log(this.devices);
-    });
+    axios
+      .get("http://localhost:8080/api/connectedDevices")
+      .then((data) => {
+        console.log(data, "received data");
+        this.devices = data.data.devices;
+        console.log(this.devices);
+      });
   },
   methods: {
-    toggle(deviceName, state) {
-      console.log(deviceName, "DeviceName", state);
-      axios.post("http://localhost:5000/changeState", {
-        MAC_ID: 1,
-        deviceName: deviceName,
-        deviceState: state == 'ON' ? 'OFF': 'ON'
-      }).then((data) => {
-      console.log(data, "received data");
-      this.devices = data.data.devices;
-      console.log(this.devices);
-    });
+    toggle(deviceName) {
+      console.log(deviceName, "DeviceName");
+      axios
+        .post("http://localhost:8080/api/sendMessage", {
+          macId: this.devices[0],
+          state: this.switch1 ? 1 : 0,
+        })
+        .then((data) => {
+          console.log(data, "received data");
+        });
+    },
+    remove(macid) {
+      axios.post("http://localhost:8080/api/deleteDevice", {
+        macid 
+      }).then((response) => {
+        console.log(response, "After delete");
+      })
+      axios
+      .get("http://localhost:8080/api/connectedDevices")
+      .then((data) => {
+        console.log(data, "received data");
+        this.devices = data.data.devices;
+        console.log(this.devices);
+      });
     }
-  }
+  },
 };
 </script>
